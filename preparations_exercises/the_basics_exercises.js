e0c7f5da5fcfb93a816d8433ad3bbae9f80ce217
Taron Avagyan <contact@taronavagyan.com>
2022-03-25 11:08:03 -0700

    completed first 7 exercises of "the basics"

---
+++ preparations_exercises/the_basics_exercises.js
@@ -0,0 +1,46 @@
+// 1.
+
+/* 'John ' + 'Doe'; */
+
+// 2.
+
+/*
+let number = 4936;
+let ones = number % 10;
+
+number = (number - ones) / 10;
+let tens = number % 10;
+
+number = (number - tens) / 10;
+let hundreds = number % 10;
+
+number = (number - hundreds) / 10;
+let thousands = number % 10;
+*/
+
+/* 3.
+
+string
+boolean
+number
+number
+undefined
+object
+
+*/
+
+// 4.
+
+/* Since one of the operands is a string,  the other operands are coerced into strings. */
+
+// 5.
+
+/* console.log(Number('5') + 10); */
+
+// 6.
+
+/* console.log(`The value of 5 + 10 is ${Number('5') + 10}.`); */
+
+// 7.
+
+/* No, it will return undefined because the value at that index is undefined. */
