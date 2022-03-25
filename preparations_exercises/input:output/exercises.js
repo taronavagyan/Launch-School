28f1484b4acee08f90debbecccb518ccad9d1aad
Taron Avagyan <contact@taronavagyan.com>
2022-03-25 13:06:45 -0700

    completed input/output exercises

---
+++ preparations_exercises/input:output/exercises.js
@@ -0,0 +1,11 @@
+// 1.
+
+/* see greeter.js */
+
+// 2.
+
+/* see greeter.js */
+
+// 3.
+
+/* see ../variables/age.js */
--- preparations_exercises/variables/age.js
+++ preparations_exercises/variables/age.js
@@ -1,4 +1,5 @@
-let age = 20;
+let rlSync = require('readline-sync');
+let age = Number(rlSync.question('How old are you? '));
 
 console.log(`You are ${age} years old.`);
 console.log(`In 10 years, you will be ${age + 10} years old.`);
