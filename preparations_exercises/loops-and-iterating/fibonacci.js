9f91e4174dd453585dca95cfb541d0e4ba147625
Taron Avagyan <contact@taronavagyan.com>
2022-03-30 10:47:11 -0700

    finished notes for "loops and iterating"

---
+++ preparations_exercises/loops-and-iterating/fibonacci.js
@@ -0,0 +1,4 @@
+function fibonacci(number) {
+    if (number < 2) return number;
+    return fibonacci(number - 1) + fibonacci(number - 2);
+}
---
+++ preparations_exercises/loops-and-iterating/names.js
@@ -0,0 +1,25 @@
+/*
+let names = ['Chris', 'Kevin', 'Naveed', 'Pete', 'Victor'];
+let upperNames = [];
+let index = 0;
+
+while (index < names.length) {
+  let upperCaseName = names[index].toUpperCase();
+  upperNames.push(upperCaseName);
+  index += 1;
+}
+
+console.log(upperNames);
+*/
+
+let names = ['Chris', 'Kevin', 'Naveed', 'Pete', 'Victor'];
+let upperNames = [];
+
+for (let index = 0; index < names.length; index += 1) {
+  if (names[index] !== 'Naveed') {
+    let upperCaseName = names[index].toUpperCase();
+    upperNames.push(upperCaseName);
+  }
+}
+
+console.log(upperNames);
