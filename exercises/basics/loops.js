af0de7e2a7979ef5a29afe5568c60aaa37745add
Taron Avagyan <contact@taronavagyan.com>
2022-04-01 16:53:07 -0700

    completed until question 3 of JS basics/loops

---
+++ exercises/basics/loops.js
@@ -0,0 +1,33 @@
+// 1.
+
+/*
+for (let i = 0; i <= 10; i += 2) {
+  console.log(i);
+}
+*/
+
+// 2.
+
+/*
+for (let i = 10; i >= 1; i -= 1) {
+  console.log(i);
+}
+
+console.log('Launch!');
+*/
+
+// 3.
+
+/*
+function repeatThreeTimes(string) {
+  for (let i = 0; i < 3; i+=1) {
+    console.log(string);
+  }
+}
+
+let greeting = 'Aloha';
+
+repeatThreeTimes(greeting);
+*/
+
+// 4.
