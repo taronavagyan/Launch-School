e33aa1006104bdcc8763b5644bab2152fce283a6
Taron Avagyan <contact@taronavagyan.com>
2022-04-02 11:10:25 -0700

    completed first 5 exercises of basics/functions1 exercises

---
+++ exercises/basics/functions1.js
@@ -0,0 +1,45 @@
+// 1.
+
+/*
+function sum(number1, number2) {
+  return number1 + number2;
+}
+
+console.log(sum(22,10));
+*/
+
+// 2.
+
+/*
+function brendanEichQuote() {
+  console.log('Always bet on JavaScript');
+}
+
+brendanEichQuote();
+The function returns undefined because it has no return statement.
+*/
+
+// 3.
+
+/*
+function cite(speaker, quote) {
+  console.log(`${speaker} said: "${quote}"`);
+}
+
+cite('Santa Claus','Ho, Ho, Ho');
+*/
+
+// 4.
+
+/*
+function squaredNumber(number) {
+  return number ** 2;
+}
+
+console.log(squaredNumber(9));
+console.log(squaredNumber(3));
+*/
+
+// 5.
+
+/* There will be no output since the function is not invoked. */
