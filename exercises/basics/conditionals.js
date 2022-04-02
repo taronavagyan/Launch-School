65967c5dd50fb473dcb36eb9d908b73113c0a397
Taron Avagyan <contact@taronavagyan.com>
2022-04-02 11:00:41 -0700

    completed exercises for basics/conditionals

---
+++ exercises/basics/conditionals.js
@@ -0,0 +1,91 @@
+// 1.
+
+/* 
+0
+null
+''
+undefined
+false
+NaN
+-0
+0n
+*/
+
+// 2.
+
+/*
+let randomNumber = Math.round(Math.random());
+if (randomNumber) {
+  console.log('Yes!');
+} else {
+  console.log('No.');
+}
+*/
+
+// 3.
+
+/*
+let randomNumber = Math.round(Math.random());
+randomNumber ? console.log('Yes!') : console.log('No.');
+console.log(randomNumber ? 'Yes!' : 'No.');
+*/
+
+// 4.
+
+/*
+let weather = "dog";
+
+if (weather === "sunny") 
+  console.log("It's a beautiful day!");
+else if (weather === "rainy")
+  console.log("Grab your umbrella.");
+else 
+  console.log("Let's stay inside.");
+*/
+
+// 5.
+
+/* 
+neigh 
+tweet tweet 
+*cricket* 
+*/
+
+// 6.
+
+/*
+let weather = "sunny";
+
+switch (weather) {
+  case "sunny":
+    console.log("It's a beautiful day!");
+    break;
+  case "rainy":
+    console.log("Grab your umbrella.");
+    break;
+  default:
+    console.log("Let's stay inside.");
+}
+*/
+
+// 7.
+
+/* Yes! because the operand is || and one of the conditions is true. */
+
+// 8.
+
+/* No... because the operand is && and one of the conditions is false. */
+
+// 9.
+
+/* $3.99 because sale returns true, but !sale returns false, so admissionPrice is assigned to the latter option. */
+
+// 10.
+
+/* 
+true
+It is not the same because without parentheses is equivalent to ((brakingForce < acceleration && speed > 0) || acceleration > 0);
+However, the expression would still evaluate to true in this situation.
+*/
+
+
