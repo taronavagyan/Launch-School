5536a75a4a6d22695bad0ef2820132b19da607e0
Taron Avagyan <contact@taronavagyan.com>
2022-04-04 21:40:07 -0700

    finished exercises for basics/arrays

--- exercises/basics/arrays.js
+++ exercises/basics/arrays.js
@@ -45,3 +45,118 @@ for (let score of scores) {
 }
 console.log(overHundred);
 */
+
+// 6.
+
+/*
+let vocabulary = [
+  ["happy", "cheerful", "merry", "glad"],
+  ["tired", "sleepy", "fatigued", "drained"],
+  ["excited", "eager", "enthused", "animated"],
+];
+
+for (let array of vocabulary) {
+  for (let word of array) {
+    console.log(word);
+  }
+}
+*/
+
+// 7.
+
+/* False because they are not the same object. */
+
+// 8.
+
+/* Use the Array.isArray() method. */
+
+// 9.
+
+/*
+let destinations = [
+  "Prague",
+  "London",
+  "Sydney",
+  "Belfast",
+  "Rome",
+  "Aruba",
+  "Paris",
+  "Bora Bora",
+  "Barcelona",
+  "Rio de Janeiro",
+  "Marrakesh",
+  "New York City",
+];
+
+function contains(string, array) {
+  for (let item of array) {
+    if (string === item) {
+      return true;
+    }
+  }
+  return false;
+}
+
+console.log(contains("Barcelona", destinations)); // true
+console.log(contains("Nashville", destinations)); // false
+
+BETTER SOLUTION: 
+
+function contains(element, list) {
+  return list.indexOf(element) >= 0;
+}
+*/
+
+// 10.
+
+/*
+let passcode = ["11", "jZ5", "hQ3f*", "8!7g3", "p3Fs"];
+
+function concatToString(array) {
+  let result = "";
+  for (let string of array) {
+    result = result.concat(string) + "-";
+  }
+  return result;
+}
+
+console.log(concatToString(passcode));
+// Expected return value: '11-jZ5-hQ3f*-8!7g3-p3Fs'
+
+BETTER SOLUTION:
+
+passcode.join('-');
+*/
+
+// 11.
+
+/*
+let groceryList = [
+  "paprika",
+  "tofu",
+  "garlic",
+  "quinoa",
+  "carrots",
+  "broccoli",
+  "hummus",
+];
+
+function logAndDelete(list) {
+  for (let element of list) {
+    console.log(element);
+  }
+  for (let i = list.length - 1; i >= 0; i -= 1) {
+    list.pop();
+  }
+}
+
+logAndDelete(groceryList);
+console.log(groceryList);
+
+BETTER SOLUTION:
+
+while (groceryList.length > 0) {
+  let checkedItem = groceryList.shift();
+  console.log(checkedItem);
+}
+*/
