578285d098f92e9005eaca6a3b9c7bbde2271245
Taron Avagyan <contact@taronavagyan.com>
2022-04-04 21:22:26 -0700

    finished first 5 exercsies of basics/arrays

---
+++ exercises/basics/arrays.js
@@ -0,0 +1,47 @@
+// 1.
+
+/*
+function first(array) {
+  return array[0];
+}
+console.log(first(["Earth", "Moon", "Sun"]));
+*/
+
+// 2.
+
+/*
+function last(array) {
+  return array[array.length - 1];
+}
+console.log(last(["Earth", "Moon", "Mars"])); // 'Mars'
+*/
+
+// 3.
+
+/*
+let energy = ["fossil", "solar", "wind", "tidal", "fusion"];
+energy.shift();
+energy.push("geothermal");
+console.log(energy);
+*/
+
+// 4.
+
+/*
+let alphabet = "abcdefghijklmnopqrstuvwxyz";
+console.log(Array.from(alphabet));
+*/
+
+// 5.
+
+/*
+let scores = [96, 47, 113, 89, 100, 102];
+let overHundred = 0;
+
+for (let score of scores) {
+  if (score >= 100) {
+    overHundred += 1;
+  }
+}
+console.log(overHundred);
+*/
