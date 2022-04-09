b31b9333a57d4c1ca34d6300bc61d9060a6e45f1
Taron Avagyan <contact@taronavagyan.com>
2022-04-08 20:53:38 -0700

    completed pseudocode exercises

---
+++ JS101/lesson_2/pseudocode_exercises.js
@@ -0,0 +1,100 @@
+// 1. a function that returns the sum of two numbers
+
+/*
+ * Get the first number
+ * Get the second number
+ * Add them together and return the sum
+ */
+
+/*
+ * START
+ * GET the first number
+ * GET the second number
+ * Add them together and return the sum
+ * END
+ */
+
+// 2. a function that takes an array of strings, and returns a string that is all those strings concatenated together
+
+/*
+ * Get the array of strings
+ * Create a "result" varialbe that is an empty string
+ * Loop through each string in the array and concat it to the result variable
+ * Return the result variable after the loop
+ */
+
+/*
+ * START
+ * GET array of strings
+ * SET result variable to ""
+ * Loop through each string of the array
+ * Concat the current string to the result variable
+ * Return the result variable after the loop
+ * END
+ */
+
+// 3. a method that takes an array of integers, and returns a new array with every other element from the original array, starting with the first element. \
+
+/*
+ * Get an array of integers
+ * Create an empty array
+ * Use a for loop to iterate over the array, starting at index 0 and increasing by 2 each iteration
+ * Add the integer at the index to the new array
+ * Return the new array
+ */
+
+/*
+ * START
+ * GET an array of integers
+ * SET the result variable to an empty array
+ * Iterate over the array with a for loop, starting at index 0 and increasing by 2 each iteration
+ * Add the integer to the new array each iteration
+ * Return the new array
+ * END
+ */
+
+// 4. a function that determines the index of the 3rd occurrence of a given character in a string. For instance, if the given character is 'x' and the string is 'axbxcdxex', the function should return 6 (the index of the 3rd 'x'). If the given character does not occur at least 3 times, return null.
+
+/*
+ * Get a string and character to track
+ * Set a "count" variable to 0
+ * Loop through the characters of the string with an indexed for loop
+ * Each iteration, check if the current character is equivalent to the desired character
+ * If it is, add 1 to count. Else, continue.
+ * If count is equal to three, return the current index
+ * If the loop has finished, return null.
+ */
+
+/*
+ * START
+ * GET string and target character
+ * SET "count" variable to 0
+ * Loop through each character of the string with a for loop
+ * IF the current character is equal to the target character
+ *  add 1 to the count.
+ *  IF the count is 3, return the index variable of the for loop.
+ * ELSE
+ *  continue.
+ * After the loop, return null.
+ * END
+ */
+
+// 5. a function that takes two arrays of numbers and returns the result of merging the arrays. The elements of the first array should become the elements at the even indexes of the returned array, while the elements of the second array should becoome the elements at the odd indexes.
+
+/*
+ * Get two arrays of integer
+ * Set a result variable to an empty array
+ * Use a for loop starting at 0 and ended at the last index of either array
+ * For both arrays and starting with the first array given, add the value at the current index to the end of the result array.
+ * Return result array.
+ */
+
+/*
+ * START
+ * GET two arrays of integers of the same length
+ * SET a result variable to an empty array
+ * Use a for loop with a counter to loop through the indexes of the arrays
+ * For both arrays and starting with the first array given, add the value at the current index to the end of the result array.
+ * Return the result array.
+ * END
+ */
