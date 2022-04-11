1764ff9d1d3d04bb1d5156e3e6d4f15111a290a2
Taron Avagyan <contact@taronavagyan.com>
2022-04-11 10:04:54 -0700

    Added pseudocode for mortgage loan calculator

---
+++ JS101/lesson_2/mortgage_loan_calculator.js
@@ -0,0 +1,11 @@
+/*
+ * START
+ * GET the loan amount
+ * GET the APR
+ * GET the loan duration in years
+ * SET loanDurationInMonths to loanDuration / 12
+ * SET monthlyInterestRate to APR / 12
+ * SET monthlyPayments
+ * RETURN monthlyInterestRate and loanDurationInMonths
+ * END
+ */
