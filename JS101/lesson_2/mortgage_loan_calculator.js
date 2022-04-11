7245f2f5bb573e1da3f4797d27ba0bfdf33aa106
Taron Avagyan <contact@taronavagyan.com>
2022-04-11 11:46:31 -0700

    added input validation and refactored code

--- JS101/lesson_2/mortgage_loan_calculator.js
+++ JS101/lesson_2/mortgage_loan_calculator.js
@@ -9,39 +9,66 @@
  * RETURN monthlyInterestRate and loanDurationInMonths
  * END
  */
+const readline = require("readline-sync");
 
 function prompt(message) {
   console.log(`=> ${message}`);
 }
 
-const readline = require("readline-sync");
+function getLoanAmount() {
+  prompt("How big of a loan would you like? (USD)");
+  let loanAmount = Number(readline.question("$"));
 
-prompt("How big of a loan would you like? (USD)");
-let loanAmount = Number(readline.question("$"));
-
-while (loanAmount <= 0 || Number.isNaN(loanAmount)) {
-  prompt("Something seems off... Try re-entering the loan amount.");
-  loanAmount = Number(readline.question("$"));
+  while (loanAmount <= 0 || Number.isNaN(loanAmount)) {
+    prompt("Something seems off... Try re-entering the loan amount.");
+    loanAmount = Number(readline.question("$"));
+  }
+  return loanAmount;
 }
 
-prompt("What is the annual interest rate (APR)?");
-let yearlyInterest = readline.question() / 100;
+function getMonthlyInterest() {
+  prompt("What is the annual interest rate (APR)?");
+  let yearlyInterest = readline.question() / 100;
 
-while (yearlyInterest < 0) {
-  prompt("Please provide a positive interest rate.");
-  yearlyInterest = readline.question() / 100;
+  while (yearlyInterest < 0) {
+    prompt("Please provide a positive interest rate.");
+    yearlyInterest = readline.question() / 100;
+  }
+  return yearlyInterest / 12;
 }
 
-prompt("How many years would you like the loan to be for?");
-let loanDurationInYears = "" + readline.question();
-//prettier-ignore
-let monthlyInterest =  yearlyInterest / 12;
+function getLoanDuration() {
+  prompt("How many years and months would you like the loan to be for?");
+  let loanDurationInYears = "" + readline.question("Years: ");
+  let loanDurationExtraMonths = "" + readline.question("Months: ");
 
-let loanDurationInMonths = loanDurationInYears * 12;
-//prettier-ignore
-let monthlyPayments = Math.round(
-  loanAmount *
-  (monthlyInterest /
-    (1 - Math.pow((1 + monthlyInterest), (-loanDurationInMonths)))));
+  //prettier-ignore
+  let loanDurationInMonths =
+    (loanDurationInYears * 12) + Number(loanDurationExtraMonths);
+  return loanDurationInMonths;
+}
 
-prompt(`Your payments for this loan would be $${monthlyPayments} per month.`);
+function repeat() {
+  prompt("Would you like to do another calculation? (y/n)");
+  if (readline.question().toLowerCase() === "y") {
+    start();
+  } else {
+    prompt("Thank you for using this loan calculator!");
+  }
+}
+
+function start() {
+  prompt("Welcome!");
+  let loanAmount = getLoanAmount();
+  let monthlyInterest = getMonthlyInterest();
+  let loanDurationInMonths = getLoanDuration();
+  //prettier-ignore
+  let monthlyPayments = Math.round(
+    loanAmount *
+    (monthlyInterest /
+      (1 - Math.pow((1 + monthlyInterest), (-loanDurationInMonths)))));
+  prompt(`Your payments for this loan would be $${monthlyPayments} per month.`);
+  repeat();
+}
+
+start();
