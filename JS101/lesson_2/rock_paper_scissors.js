f4d93f7c275433e6ceb00f556941e67a818200d2
Taron Avagyan <contact@taronavagyan.com>
2022-04-12 22:02:29 -0700

    added pseudocode

---
+++ JS101/lesson_2/rock_paper_scissors.js
@@ -0,0 +1,16 @@
+/*
+ * START
+ * GET userInput
+ * Validate input
+ * GET random computerInput
+ * If player a chooses rock and computer chooses scissors, player a wins.
+ * If player a chooses paper and computer chooses rock, player a wins.
+ * If player a chooses scissors and computer chooses paper, player a wins.
+ * If both players choose the same item, neither one wins. It's a tie.
+ * Display winner
+ * Ask user if they'd like to play again
+ * Validate input
+ * If yes, restart
+ * If no, say thank you
+ * END
+ */
