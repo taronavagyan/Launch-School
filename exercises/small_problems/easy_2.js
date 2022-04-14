aa298df1e9e0249366f5a31db32ac10c21a2cb2d
Taron Avagyan <contact@taronavagyan.com>
2022-04-14 09:32:05 -0700

    completed 4 and 5

--- exercises/small_problems/easy_2.js
+++ exercises/small_problems/easy_2.js
@@ -36,3 +36,51 @@ function multiply(num1, num2) {
 
 console.log(multiply(3, 5) === 15);
 */
+
+// 4.
+
+/*
+const multiply = (num1, num2) => num1 * num2;
+
+const square = (num) => multiply(num, num);
+
+console.log(square(5) === 25); // logs true
+console.log(square(-8) === 64); // logs true
+
+function powerOfN(num, n) {
+  let currNumber = num;
+  for (let count = 1; count < n; count += 1) {
+    currNumber = multiply(currNumber, num);
+  }
+  return currNumber;
+}
+
+console.log(powerOfN(2, 10));
+*/
+
+// 5.
+
+/*
+const readline = require("readline-sync");
+
+function prompt(msg) {
+  console.log(`==> ${msg}`);
+}
+
+function doAllOperations(num1, num2) {
+  prompt(`${num1} + ${num2} = ${num1 + num2}`);
+  prompt(`${num1} - ${num2} = ${num1 - num2}`);
+  prompt(`${num1} * ${num2} = ${num1 * num2}`);
+  prompt(`${num1} / ${num2} = ${Math.floor(num1 / num2)}`);
+  prompt(`${num1} % ${num2} = ${num1 % num2}`);
+  prompt(`${num1} ** ${num2} = ${num1 ** num2}`);
+}
+
+prompt("Enter the first number: ");
+let num1 = Number(readline.question());
+
+prompt("Enter the second number: ");
+let num2 = Number(readline.question());
+
+doAllOperations(num1, num2);
+*/
