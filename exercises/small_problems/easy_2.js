e310f6630ef9cbdd3770671b955c1914a82f9054
Taron Avagyan <contact@taronavagyan.com>
2022-04-14 09:16:04 -0700

    completed 2 and 3

--- exercises/small_problems/easy_2.js
+++ exercises/small_problems/easy_2.js
@@ -13,3 +13,26 @@ console.log(
 */
 
 // 2.
+
+/*
+const readline = require("readline-sync");
+
+let name = readline.question("What is your name? ");
+
+if (name[name.length - 1] === "!") {
+  name = name.slice(0, -1);
+  console.log(`HELLO ${name.toUpperCase()}. WHY ARE WE SCREAMING?`);
+} else {
+  console.log(`Hello ${name}. Nice to meet you!`);
+}
+*/
+
+// 3.
+
+/*
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+console.log(multiply(3, 5) === 15);
+*/
