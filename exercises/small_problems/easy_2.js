108894eb2b8a6effb711a8b0e0217249fb1c7828
Taron Avagyan <contact@taronavagyan.com>
2022-04-13 21:52:32 -0700

    finished problem 1

---
+++ exercises/small_problems/easy_2.js
@@ -0,0 +1,15 @@
+// 1.
+
+/*
+function greetings(name, titleAndOccupation) {
+  let fullName = name.join(" ");
+  let fullTitle = Object.values(titleAndOccupation).join(" ");
+  return `Greetings, ${fullName}! I hear you're a ${fullTitle}. Impressive!`;
+}
+
+console.log(
+  greetings(["John", "Q", "Doe"], { title: "Master", occupation: "Plumber" })
+);
+*/
+
+// 2.
