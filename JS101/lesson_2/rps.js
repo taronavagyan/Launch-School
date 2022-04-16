c43baed4ade82fd78be4daa698258521977a1ecc
Taron Avagyan <contact@taronavagyan.com>
2022-04-16 10:04:20 -0700

    added game functionality

--- JS101/lesson_2/rps.js
+++ JS101/lesson_2/rps.js
@@ -10,12 +10,46 @@ const VALID_CHOICES = ["rock", "paper", "scissors"];
 function prompt(msg) {
   console.log(`=> ${msg}`);
 }
-prompt(`Choose one: ${VALID_CHOICES.join(", ")}`);
-let choice = readline.question();
 
-while (!VALID_CHOICES.includes(choice.toLowerCase())) {
-  prompt("That's not a valid choice.");
-  choice = readline.question();
+while (true) {
+  prompt(`Choose one: ${VALID_CHOICES.join(", ")}`);
+  let choice = readline.question();
+
+  while (!VALID_CHOICES.includes(choice.toLowerCase())) {
+    prompt("That's not a valid choice.");
+    choice = readline.question();
+  }
+
+  let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
+  let computerChoice = VALID_CHOICES[randomIndex];
+
+  prompt(`You chose ${choice}. Computer chose ${computerChoice}.`);
+
+  if (
+    (choice === "rock" && computerChoice === "scissors") ||
+    (choice === "scissors" && computerChoice === "paper") ||
+    (choice === "paper" && computerChoice === "rock")
+  ) {
+    prompt("You won!");
+  } else if (
+    (choice === "scissors" && computerChoice === "rock") ||
+    (choice === "paper" && computerChoice === "scissors") ||
+    (choice === "rock" && computerChoice === "paper")
+  ) {
+    prompt("Computer won!");
+  } else {
+    prompt("It's a tie!");
+  }
+
+  prompt("Would you like to play again? (y/n)");
+  let answer = readline.question().toLowerCase();
+
+  while (answer[0] !== "y" && answer[0] !== "n") {
+    prompt("Please enter 'y' or 'n'");
+    answer = readline.question().toLowerCase();
+  }
+
+  if (answer[0] !== "y") {
+    break;
+  }
 }
-
-console.log(Math.floor(Math.random() * VALID_CHOICES.length));
