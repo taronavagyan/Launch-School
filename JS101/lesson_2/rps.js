c70e320a0755df414e020c1c3d57f251b3ab585e
Taron Avagyan <contact@taronavagyan.com>
2022-04-15 23:12:48 -0700

    added user input and framework for game

--- JS101/lesson_2/rps.js
+++ JS101/lesson_2/rps.js
@@ -3,3 +3,19 @@
  * the computer will choose their move
  * display who won/the result
  */
+
+const readline = require("readline-sync");
+const VALID_CHOICES = ["rock", "paper", "scissors"];
+
+function prompt(msg) {
+  console.log(`=> ${msg}`);
+}
+prompt(`Choose one: ${VALID_CHOICES.join(", ")}`);
+let choice = readline.question();
+
+while (!VALID_CHOICES.includes(choice.toLowerCase())) {
+  prompt("That's not a valid choice.");
+  choice = readline.question();
+}
+
+console.log(Math.floor(Math.random() * VALID_CHOICES.length));
