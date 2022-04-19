05eb8c4853e124479185fa2c75efffc00985ae3b
Taron Avagyan <contact@taronavagyan.com>
2022-04-19 10:56:22 -0700

    completed #3

--- exercises/small_problems/easy_3.js
+++ exercises/small_problems/easy_3.js
@@ -22,6 +22,7 @@ crunch(""); // ""
 
 // 2.
 
+/*
 // eslint-disable-next-line max-lines-per-function
 function logInBox(text, maxLength) {
   //prettier-ignore
@@ -57,6 +58,23 @@ function logInBox(text, maxLength) {
 }
 
 logInBox(
+  // eslint-disable-next-line max-len
   "For a challenging but fun exercise, try word wrapping messages that are too long to fit, so that they appear on multiple lines but are still contained within the box. This isn't an easy problem, but it's doable with basic JavaScript.",
   50
 );
+*/
+
+//3
+
+/*
+function stringy(int) {
+  let stringyString = "";
+  for (let count = 1; count <= int; count += 1) {
+    let number = count % 2 === 1 ? 1 : 0;
+    stringyString += number;
+  }
+  return stringyString;
+}
+
+console.log(stringy(6));
+*/
