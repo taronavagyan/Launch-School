d0a41d8d723acd6f652e9c0ac62377a807023329
Taron Avagyan <contact@taronavagyan.com>
2022-04-19 10:04:12 -0700

    added length validation

--- exercises/small_problems/easy_3.js
+++ exercises/small_problems/easy_3.js
@@ -22,15 +22,35 @@ crunch(""); // ""
 
 // 2.
 
-function logInBox(text) {
-  let topAndBottom = `+${"-".repeat(text.length + 2)}+`;
-  let emptyLine = `|${" ".repeat(text.length + 2)}|`;
+// eslint-disable-next-line max-lines-per-function
+function logInBox(text, maxLength) {
+  //prettier-ignore
+  let length = (text.length + 2) > maxLength ? maxLength : (text.length + 2);
+  let topAndBottom = `+${"-".repeat(length)}+`;
+  let emptyLine = `|${" ".repeat(length)}|`;
+  if (text.length > maxLength) emptyLine = maxLength;
+  let words = text.split(" ");
+  let lineLength = 0;
+  let lineBreaks = [];
+
+  for (let word of words) {
+    if (lineLength + word.length >= maxLength) {
+      lineBreaks.push(word);
+      lineLength = 0;
+    } else {
+      lineLength += word.length;
+    }
+  }
 
   console.log(topAndBottom);
+
   console.log(emptyLine);
   console.log(`| ${text} |`);
   console.log(emptyLine);
   console.log(topAndBottom);
 }
 
-logInBox("i love to go to 6flags");
+logInBox(
+  "For a challenging but fun exercise, try word wrapping messages that are too long to fit, so that they appear on multiple lines but are still contained within the box. This isn't an easy problem, but it's doable with basic JavaScript",
+  50
+);
