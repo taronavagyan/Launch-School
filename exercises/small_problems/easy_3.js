8b9885be0c4e5147cf41c8ff6013b5d3c2adecdd
Taron Avagyan <contact@taronavagyan.com>
2022-04-18 20:16:22 -0700

    1st try at #2

--- exercises/small_problems/easy_3.js
+++ exercises/small_problems/easy_3.js
@@ -19,3 +19,23 @@ crunch("ggggggggggggggg"); // "g"
 crunch("a"); // "a"
 crunch(""); // ""
 */
+
+// 2.
+
+/*
+function logInBox(text) {
+  let bars = "";
+  let emptySpace = "";
+  for (let count = 0; count < text.length; count++) {
+    bars = bars.concat("-");
+    emptySpace = emptySpace.concat(" ");
+  }
+  console.log(`+ ${bars} +`);
+  console.log(`| ${emptySpace} |`);
+  console.log(`| ${text} |`);
+  console.log(`| ${emptySpace} |`);
+  console.log(`+ ${bars} +`);
+}
+
+logInBox("i love to go to 6flags");
+*/
