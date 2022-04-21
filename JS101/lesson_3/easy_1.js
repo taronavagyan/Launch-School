ea13a2f2d005d52ef3562cec4b70ba8549f1f764
Taron Avagyan <contact@taronavagyan.com>
2022-04-21 12:51:17 -0700

    completed 1-5

---
+++ JS101/lesson_3/easy_1.js
@@ -0,0 +1,53 @@
+/* eslint-disable max-len */
+// 1.
+
+/*
+let numbers = [1, 2, 3];
+numbers[6] = 5;
+
+// This code will not raise an error. numbers[3-5] are empty slots
+
+numbers[4]; // what will this line return?
+
+// undefined. BUT, it is an empty slot.
+*/
+
+// 2.
+
+/*
+let str1 = "Come over here!"; // true
+let str2 = "What's up, Doc?"; // false
+
+console.log(str1.endsWith("!"));
+console.log(str2.endsWith("!"));
+*/
+
+// 3.
+
+/*
+let ages = { Herman: 32, Lily: 30, Grandpa: 402, Eddie: 10 };
+
+console.log(ages.hasOwnProperty("Spot"));
+*/
+
+// 4.
+
+/*
+let munstersDescription = "the Munsters are CREEPY and Spooky.";
+// => The munsters are creepy and spooky.
+
+function firstCharCapitalOnly(string) {
+  return string[0].toUpperCase() + string.substring(1).toLowerCase();
+}
+
+console.log(firstCharCapitalOnly(munstersDescription));
+*/
+
+// 5.
+
+/*
+console.log(false == "0"); //true
+console.log(false === "0"); //false
+*/
+
+// 6.
