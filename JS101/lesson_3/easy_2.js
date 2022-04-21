71d4da0125976c338160cb9a00b0ea56bf95a581
Taron Avagyan <contact@taronavagyan.com>
2022-04-21 16:17:50 -0700

    finished #4

--- JS101/lesson_3/easy_2.js
+++ JS101/lesson_3/easy_2.js
@@ -28,3 +28,15 @@ console.log(numbersReversed2);
 console.log(numbersReversed3);
 console.log(numbers);
 */
+
+// 4.
+
+/*
+let numbers = [1, 2, 3, 4, 5, 15, 16, 17, 95, 96, 99];
+
+let number1 = 8; // false
+let number2 = 95; // true
+
+console.log(numbers.includes(number1));
+console.log(numbers.includes(number2));
+*/
