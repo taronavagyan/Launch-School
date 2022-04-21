8ab8de28c52aa179636fff8892a22264b80409cd
Taron Avagyan <contact@taronavagyan.com>
2022-04-21 16:16:57 -0700

    completed #2

---
+++ JS101/lesson_3/easy_2.js
@@ -0,0 +1,30 @@
+// 1.
+
+/*
+let advice =
+  "Few things in life are as important as house training important your pet dinosaur.";
+
+console.log(advice.replaceAll("important", "urgent"));
+*/
+
+// 2.
+
+/*
+let numbers = [1, 2, 3, 4, 5];
+let numbersReversed1 = numbers.slice().reverse();
+
+let numbersReversed2 = [...numbers].sort(
+  (firstNum, secondNum) => secondNum - firstNum
+);
+
+let numbersReversed3 = [];
+
+numbers.forEach((num) => {
+  numbersReversed3.unshift(num);
+});
+
+console.log(numbersReversed1);
+console.log(numbersReversed2);
+console.log(numbersReversed3);
+console.log(numbers);
+*/
