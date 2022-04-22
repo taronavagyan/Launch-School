ad09728132c77fd7e716f209b2f28429faaa9d3b
Taron Avagyan <contact@taronavagyan.com>
2022-04-21 17:28:54 -0700

    finished #8

--- JS101/lesson_3/easy_2.js
+++ JS101/lesson_3/easy_2.js
@@ -101,3 +101,13 @@ let barneyArray3 = flintstonesPairs
   .shift();
 console.log(barneyArray3);
 */
+
+// 8.
+
+/*
+let numbers = [1, 2, 3, 4]; // true
+let table = { field1: 1, field2: 2, field3: 3, field4: 4 }; // false
+
+console.log(Array.isArray(numbers));
+console.log(Array.isArray(table));
+*/
