a3dfda2ce63f1ef4a3d6da60663e82008f7cbfbe
Taron Avagyan <contact@taronavagyan.com>
2022-04-21 17:09:45 -0700

    completed #6

--- JS101/lesson_3/easy_2.js
+++ JS101/lesson_3/easy_2.js
@@ -61,3 +61,15 @@ arrayOfNums = arrayOfNums.slice(0, 2).concat(arrayOfNums.slice(3));
 
 console.log(arrayOfNums);
 */
+
+// 6.
+
+/*
+let flintstones = ["Fred", "Wilma"];
+flintstones.push(["Barney", "Betty"]);
+flintstones.push(["Bambam", "Pebbles"]);
+
+let flintstonesFlattened = flintstones.flat();
+
+console.log(flintstonesFlattened);
+*/
