c918c641360ee9e20da218dc03bed79fa9891cf0
Taron Avagyan <contact@taronavagyan.com>
2022-04-22 14:49:33 -0700

    finished #3

--- JS101/lesson_3/medium_1.js
+++ JS101/lesson_3/medium_1.js
@@ -28,3 +28,23 @@ function swapCasing(text) {
 
 console.log(swapCasing(munstersDescription));
 */
+
+// 3.
+
+/*
+function factors(number) {
+  let divisor = number;
+  let factors = [];
+  while (divisor > 0) {
+    if (number % divisor === 0) {
+      factors.push(number / divisor);
+    }
+    divisor -= 1;
+  }
+  return factors;
+}
+
+console.log(factors(-5));
+
+// number % divisor === 0 returns true if divisor is a factor of number
+*/
