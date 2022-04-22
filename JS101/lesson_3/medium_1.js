b1eeebb4fa29e68153154299214133bb45a5c4ed
Taron Avagyan <contact@taronavagyan.com>
2022-04-22 14:37:04 -0700

    finished #1

---
+++ JS101/lesson_3/medium_1.js
@@ -0,0 +1,11 @@
+// 1.
+
+/*
+function tenXWithIndents(string) {
+  for (let padding = 0; padding < 10; padding += 1) {
+    console.log(" ".repeat(padding) + string);
+  }
+}
+
+tenXWithIndents("The Flintstones Rock!");
+*/
