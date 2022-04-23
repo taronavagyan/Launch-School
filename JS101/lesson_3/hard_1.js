6054507f274ab50f95ef2f014804221a8667ce22
Taron Avagyan <contact@taronavagyan.com>
2022-04-23 16:53:59 -0700

    finished #3

--- JS101/lesson_3/hard_1.js
+++ JS101/lesson_3/hard_1.js
@@ -10,3 +10,26 @@ return the object.
 /*
 { first: [ 1, 2 ] }
 */
+
+// 3.
+
+/*
+
+A)
+
+one is: one
+two is: two
+three is: three
+
+B)
+
+one is: one
+two is: two
+three is: three
+
+C)
+
+one is: two
+two is: three
+three is: one
+*/
