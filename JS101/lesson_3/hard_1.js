3a9754082dd5a0c275f43598b2ea2c0c1c42812f
Taron Avagyan <contact@taronavagyan.com>
2022-04-23 16:56:51 -0700

    completed all

--- JS101/lesson_3/hard_1.js
+++ JS101/lesson_3/hard_1.js
@@ -33,3 +33,20 @@ one is: two
 two is: three
 three is: one
 */
+
+// 4.
+
+/*
+function isDotSeparatedIpAddress(inputString) {
+  let dotSeparatedWords = inputString.split(".");
+  if (dotSeparatedWords.length !== 4) return false;
+  while (dotSeparatedWords.length > 0) {
+    let word = dotSeparatedWords.pop();
+    if (!isAnIpNumber(word)) {
+      return false;
+    }
+  }
+
+  return true;
+}
+*/
