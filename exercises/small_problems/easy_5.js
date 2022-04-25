a9f1f9c9b1b62bc962baf2a1ea2553871a20ca37
Taron Avagyan <contact@taronavagyan.com>
2022-04-25 16:12:26 -0700

    completed #10

--- exercises/small_problems/easy_5.js
+++ exercises/small_problems/easy_5.js
@@ -195,3 +195,16 @@ function logOccurences(occurences) {
 }
 countOccurrences(vehicles);
 */
+
+// 10.
+
+/*
+const average = (arrayOfInts) =>
+  Math.floor(
+    arrayOfInts.reduce((runningSum, currInt) => runningSum + currInt, 0) /
+      arrayOfInts.length
+  );
+
+console.log(average([1, 5, 87, 45, 8, 8]));
+console.log(average([9, 47, 23, 95, 16, 52]));
+*/
