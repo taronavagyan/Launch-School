e261e2ae0cf029272e3a875683f03f6f61b8f8ca
Taron Avagyan <contact@taronavagyan.com>
2022-04-26 16:50:58 -0700

    finished #1

---
+++ exercises/small_problems/easy_6.js
@@ -0,0 +1,13 @@
+// 1.
+
+/*
+function repeater(string) {
+  let chars = string.split("");
+  let doubledLetters = chars.map((char) => char + char);
+  return doubledLetters.join("");
+}
+
+repeater("Hello"); // "HHeelllloo"
+repeater("Good job!"); // "GGoooodd  jjoobb!!"
+repeater(""); // ""
+*/
