7284a3a2563144355247731cdee4f23ecf763829
Taron Avagyan <contact@taronavagyan.com>
2022-04-26 17:01:40 -0700

    finished #2

--- exercises/small_problems/easy_6.js
+++ exercises/small_problems/easy_6.js
@@ -11,3 +11,26 @@ repeater("Hello"); // "HHeelllloo"
 repeater("Good job!"); // "GGoooodd  jjoobb!!"
 repeater(""); // ""
 */
+
+// 2.
+
+/*
+function doubleConsonants(string) {
+  let letters = string.split("");
+  let result = "";
+
+  for (let letter of letters) {
+    if (!["a", "e", "i", "o", "u"].includes(letter.toLowerCase())) {
+      result += letter;
+    }
+    result += letter;
+  }
+
+  return result;
+}
+
+console.log(doubleConsonants("String"));
+console.log(doubleConsonants("Hello-World!"));
+console.log(doubleConsonants("July 4th"));
+console.log(doubleConsonants(""));
+*/
