fdf1d019dd49c988167040b742a7feccbae0f90c
Taron Avagyan <contact@taronavagyan.com>
2022-04-27 14:34:47 -0700

    Added notes

--- JS101/lesson_4/collections_basics.js
+++ JS101/lesson_4/collections_basics.js
@@ -8,3 +8,24 @@ console.log(arr.slice(2, 5)[0]);
 
 arr.join("");
 */
+
+let numbers = [1, 2, 3, 4];
+numbers[0] += 1; // => 2
+console.log(numbers); // => [ 2, 2, 3, 4 ]
+numbers[1] += 1;
+numbers[2] += 1;
+numbers[3] += 1;
+numbers[4] += 1;
+console.log(numbers); // => [ 2, 3, 4, 5, NaN]
+
+let obj = {
+  apple: "Produce",
+  carrot: "Produce",
+  pear: "Produce",
+  broccoli: "Produce",
+};
+obj["apple"] = "Fruit";
+obj.carrot = "Vegetable";
+obj.pear = "Fruit";
+obj["broccoli"] = "Vegetable";
+console.log(obj);
