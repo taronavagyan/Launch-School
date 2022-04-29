44411d982305b6ffcd52bfe5616252146bce871f
Taron Avagyan <contact@taronavagyan.com>
2022-04-29 09:13:34 -0700

    added notes

--- JS101/lesson_4/pedac_notes.js
+++ JS101/lesson_4/pedac_notes.js
@@ -1,4 +1,24 @@
-// 1.
+// PROBLEM:
+
+// Given a string, write a function `palindromeSubstrings` which returns
+// all the substrings from a given string which are palindromes. Consider
+// palindrome words case sensitive.
+
+// Test cases:
+
+// console.log(palindromeSubstrings("supercalifragilisticexpialidocious"))
+// should return: ["ili"]
+//
+// console.log(palindromeSubstrings("abcddcbA"))
+// should return: ["bcddcb", "cddc", "dd"]
+//
+// console.log(palindromeSubstrings("palindrome"))
+// should log: []
+//
+// console.log(palindromeSubstrings(""))
+// should log: []
+
+////////
 
 // what if input isn't a string?
 
