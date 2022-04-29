ff8ede2d87ae79a9addf1dc0e33907e608cba034
Taron Avagyan <contact@taronavagyan.com>
2022-04-29 09:12:26 -0700

    Added notes

---
+++ JS101/lesson_4/pedac_notes.js
@@ -0,0 +1,15 @@
+// 1.
+
+// what if input isn't a string?
+
+// input: string
+// output: array of substring palindromes
+
+// rules:
+//   explicit: return all substring palindromes
+//             case sensitive
+//             function called palindromeSubstrings
+
+//   implicit: return in an array
+//             empty string returns empty array
+//
