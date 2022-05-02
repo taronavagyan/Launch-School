12770f526dfd4c55bf40e145228d3e94c7b278a9
Taron Avagyan <contact@taronavagyan.com>
2022-05-02 08:50:22 -0700

    renamed variable

--- JS101/lesson_4/double_numbers.js
+++ JS101/lesson_4/double_numbers.js
@@ -1,7 +1,8 @@
-function doubleNumbers(numList) {
-  for (let count = 0; count < numList.length; count += 1) {
-    numList[count] *= 2;
+function doubleNumbers(numbers) {
+  for (let count = 0; count < numbers.length; count += 1) {
+    numbers[count] *= 2;
   }
+  return numbers;
 }
 
 let myNumbers = [1, 4, 3, 7, 2, 6];
