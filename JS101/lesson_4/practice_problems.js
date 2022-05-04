2f6e51989800909d5381c0c35a49d5508d5fa138
Taron Avagyan <contact@taronavagyan.com>
2022-05-04 10:07:44 -0700

    #3

--- JS101/lesson_4/practice_problems.js
+++ JS101/lesson_4/practice_problems.js
@@ -10,3 +10,10 @@
 [ undefined, undefined, undefined ]
 Because there are brackets, but no return statement.
 */
+
+// 3.
+
+/*
+[ 1, 4, 9 ]
+Because there are no brackets surrounding the arrow function
+*/
