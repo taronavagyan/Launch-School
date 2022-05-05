65d5be63656e3f8317eb07897eed974685e2658c
Taron Avagyan <contact@taronavagyan.com>
2022-05-05 09:49:49 -0700

    #1

---
+++ exercises/small_problems/list_processing.js
@@ -0,0 +1,16 @@
+// 1.
+
+/*
+function sum(number) {
+  let arrayOfDigits = String(number).split("");
+
+  return arrayOfDigits.reduce(
+    (prevDigit, currDigit) => Number(prevDigit) + Number(currDigit),
+    0
+  );
+}
+
+console.log(sum(23)); // 5
+console.log(sum(496)); // 19
+console.log(sum(123456789)); // 45
+*/
