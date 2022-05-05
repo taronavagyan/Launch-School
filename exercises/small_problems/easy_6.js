43ddbfd58994b4daa2236278fd23105e12918cd9
Taron Avagyan <contact@taronavagyan.com>
2022-05-04 19:34:47 -0700

    #11

--- exercises/small_problems/easy_6.js
+++ exercises/small_problems/easy_6.js
@@ -168,3 +168,36 @@ console.log(reverseWords("Professional")); // "lanoisseforP"
 console.log(reverseWords("Walk around the block")); // "Walk dnuora the kcolb"
 console.log(reverseWords("Launch School")); // "hcnuaL loohcS"
 */
+
+// 11.
+
+function reverseArray(arr) {
+  let reversedArr = [];
+
+  for (let idx = arr.length - 1; idx >= 0; idx -= 1) {
+    reversedArr.push(arr[idx]);
+  }
+
+  arr.map((_, idx) => (arr[idx] = reversedArr[idx]));
+  return arr;
+}
+
+let list = [1, 2, 3, 4];
+let result = reverseArray(list);
+console.log(result); // logs [4,3,2,1]
+console.log(list === result); // logs true
+
+let list1 = ["a", "b", "c", "d", "e"];
+let result1 = reverseArray(list1);
+console.log(result1); // logs  ["e", "d", "c", "b", "a"]
+console.log(list1 === result1); // logs true
+
+let list2 = ["abc"];
+let result2 = reverseArray(list2);
+console.log(result2); // logs  ["abc"]
+console.log(list2 === result2); // logs true
+
+let list3 = [];
+let result3 = reverseArray(list3);
+console.log(result3); // logs []
+console.log(list3 === result3); // logs true
