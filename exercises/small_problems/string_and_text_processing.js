5a003cb1da61b4f0d57cc8b0e0ed2b93ab12d0d5
Taron Avagyan <contact@taronavagyan.com>
2022-05-07 10:37:31 -0700

    #1

---
+++ exercises/small_problems/string_and_text_processing.js
@@ -0,0 +1,14 @@
+// 1.
+
+/*
+function isUppercase(string) {
+  return string === string.toUpperCase();
+}
+
+console.log(isUppercase("t"));
+console.log(isUppercase("T"));
+console.log(isUppercase("Four Score"));
+console.log(isUppercase("FOUR SCORE"));
+console.log(isUppercase("4SCORE!"));
+console.log(isUppercase(""));
+*/
