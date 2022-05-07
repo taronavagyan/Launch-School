3b90b690daa24d5cfa3a59b4dac40105a17f74c8
Taron Avagyan <contact@taronavagyan.com>
2022-05-07 09:31:58 -0700

    added notes

--- JS101/lesson_5/sort.js
+++ JS101/lesson_5/sort.js
@@ -3,3 +3,15 @@ console.log(["arc", "bat", "cape", "ants", "cap"].sort());
 
 // [ 'ants', 'arc', 'bat', 'cap', 'cape' ]
 */
+
+/*
+[2, 11, 9, 4, 107, 21, 1].sort((a, b) => {
+  if (a < b) {
+    return -1;
+  } else if (a > b) {
+    return 1;
+  } else {
+    return 0;
+  }
+}); // => [ 1, 2, 4, 9, 11, 21, 107 ]
+*/
