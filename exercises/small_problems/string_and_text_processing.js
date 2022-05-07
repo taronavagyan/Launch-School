5378540f27358fca69cb085340d1963db5000211
Taron Avagyan <contact@taronavagyan.com>
2022-05-07 10:45:57 -0700

    #2

--- exercises/small_problems/string_and_text_processing.js
+++ exercises/small_problems/string_and_text_processing.js
@@ -12,3 +12,24 @@ console.log(isUppercase("FOUR SCORE"));
 console.log(isUppercase("4SCORE!"));
 console.log(isUppercase(""));
 */
+
+// 2.
+
+// GET array of strings
+// SET result array to empty array
+// For each string
+//   replace a,e,i,o,u with empty space and push to result
+// RETURN array of strings
+
+/*
+function removeVowels(arrayOfStrings) {
+  return arrayOfStrings.map((string) => string.replace(/[a,e,i,o,u]/gi, ""));
+}
+
+console.log(removeVowels(["abcdefghijklmnopqrstuvwxyz"]));
+// ["bcdfghjklmnpqrstvwxyz"]
+console.log(removeVowels(["green", "YELLOW", "black", "white"]));
+// ["grn", "YLLW", "blck", "wht"]
+console.log(removeVowels(["ABC", "AEIOU", "XYZ"]));
+// ["BC", "", "XYZ"]
+*/
