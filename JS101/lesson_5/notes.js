b4f710579a397fa98f398841378821b55e1c82f7
Taron Avagyan <contact@taronavagyan.com>
2022-05-10 20:33:18 -0700

    notes

--- JS101/lesson_5/notes.js
+++ JS101/lesson_5/notes.js
@@ -29,3 +29,21 @@ let arr = [{ b: 'foo' }, ['bar']];
 let serializedArr = JSON.stringify(arr);
 let deepCopiedArr = JSON.parse(serializedArr);
 */
+
+// FREEZING OBJECTS
+
+/*
+let obj = Object.freeze({ a: 'foo' });
+let arr = Object.freeze(['a', 'b', 'c']);
+
+obj['b'] = 'bar';
+obj; // => { a: 'foo' }
+
+obj.c = 'ccc';
+obj; // => { a: 'foo' }
+
+arr[3] = 'x';
+arr; // => [ 'a', 'b', 'c' ]
+
+arr.push('d'); // => TypeError: Cannot add property 3, object is not extensible
+*/
