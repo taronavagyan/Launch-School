8f1bf9e4acdbb53e483707f32ba08b6d36809f84
Taron Avagyan <contact@taronavagyan.com>
2022-05-10 20:28:49 -0700

    notes

--- JS101/lesson_5/notes.js
+++ JS101/lesson_5/notes.js
@@ -3,3 +3,11 @@ let arr = [[1, 3], [2]];
 arr[1] = 'hi there';
 console.log(arr); // => [ [1, 3 ], 'hi there' ]
 */
+
+/*
+let arr = [{ a: "ant" }, { b: "bear" }];
+
+arr[0]["c"] = "cat";
+arr[0].d = "dog";
+console.log(arr); // => [{ a: 'ant', c: 'cat', d: 'dog' }, { b: 'bear' }]
+*/
