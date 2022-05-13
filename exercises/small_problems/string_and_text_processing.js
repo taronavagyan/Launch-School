2c9587615d8814a8e7d6d58898145f1a07adc854
Taron Avagyan <contact@taronavagyan.com>
2022-05-13 11:20:11 -0700

    #8

--- exercises/small_problems/string_and_text_processing.js
+++ exercises/small_problems/string_and_text_processing.js
@@ -173,11 +173,29 @@ console.log(
 //   push (word + " " wordlength[currentindex]) to result
 // RETURN result
 
-function wordLengths(string) {
-  let result = [];
+/*
+function wordLengths(string = []) {
+  if (string.length === 0) {
+    return [];
+  }
+
   let words = string.split(" ");
-  let wordLengths = words.map((word) => word.length);
-  console.log(wordLengths);
+  let result = words.map((word) => word + " " + word.length);
+  return result;
 }
 
-wordLengths("horses are cool");
+wordLengths("cow sheep chicken");
+// ["cow 3", "sheep 5", "chicken 7"]
+
+wordLengths("baseball hot dogs and apple pie");
+// ["baseball 8", "hot 3", "dogs 4", "and 3", "apple 5", "pie 3"]
+
+wordLengths("It ain't easy, is it?");
+// ["It 2", "ain't 5", "easy, 5", "is 2", "it? 3"]
+
+wordLengths("Supercalifragilisticexpialidocious");
+// ["Supercalifragilisticexpialidocious 34"]
+
+wordLengths(""); // []
+wordLengths(); // []
+*/
