4d85cd053002124fa8f282a58895576abfe0c25b
Taron Avagyan <contact@taronavagyan.com>
2022-05-13 10:23:34 -0700

    notes added

--- JS101/lesson_5/notes.js
+++ JS101/lesson_5/notes.js
@@ -47,3 +47,19 @@ arr; // => [ 'a', 'b', 'c' ]
 
 arr.push('d'); // => TypeError: Cannot add property 3, object is not extensible
 */
+
+/*
+let obj = Object.freeze({ a: "foo" });
+let arr = Object.freeze(["a", "b", "c"]);
+
+obj["b"] = "bar";
+obj; // => { a: 'foo' }
+
+obj.c = "ccc";
+obj; // => { a: 'foo' }
+
+arr[3] = "x";
+arr; // => [ 'a', 'b', 'c' ]
+
+arr.push("d"); // => TypeError: Cannot add property 3, object is not extensible
+*/
