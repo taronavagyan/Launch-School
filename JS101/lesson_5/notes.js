301c3a0b4a499f7a2fba8aacb0d944dddf8ca5e2
Taron Avagyan <contact@taronavagyan.com>
2022-05-14 15:28:02 -0700

    notes

--- JS101/lesson_5/notes.js
+++ JS101/lesson_5/notes.js
@@ -63,3 +63,24 @@ arr; // => [ 'a', 'b', 'c' ]
 
 arr.push("d"); // => TypeError: Cannot add property 3, object is not extensible
 */
+
+/*
+// for loop transformation
+let numbers = [1, 2, 3, 4, 5];
+let transformedNumbers = [];
+
+for (let index = 0; index < numbers.length; index += 1) {
+  let currentNum = numbers[index];
+  let squaredNum = currentNum * currentNum;
+
+  transformedNumbers.push(squaredNum);
+}
+
+transformedNumbers; // => [ 1, 4, 9, 16, 25 ]
+
+// map transformation
+let numbers = [1, 2, 3, 4, 5];
+let transformedNumbers = numbers.map(currentNum => currentNum * currentNum);
+
+transformedNumbers; // => [ 1, 4, 9, 16, 25 ]
+*/
