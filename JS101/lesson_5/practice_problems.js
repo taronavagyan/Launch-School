61877ab234e98814fcd0a4a0e9732b3a4e7563a0
Taron Avagyan <contact@taronavagyan.com>
2022-05-16 17:53:52 -0700

    #2

--- JS101/lesson_5/practice_problems.js
+++ JS101/lesson_5/practice_problems.js
@@ -5,3 +5,25 @@ let arr = ["10", "11", "9", "7", "8"];
 
 console.log([...arr].sort((a, b) => b - a));
 */
+
+// 2.
+
+/*
+let books = [
+  {
+    title: "One Hundred Years of Solitude",
+    author: "Gabriel Garcia Marquez",
+    published: "1967",
+  },
+  {
+    title: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
+    published: "1925",
+  },
+  { title: "War and Peace", author: "Leo Tolstoy", published: "1869" },
+  { title: "Ulysses", author: "James Joyce", published: "1922" },
+  { title: "The Book of Kells", author: "Multiple Authors", published: "800" },
+];
+
+console.log(books.sort((a, b) => a.published - b.published));
+*/
