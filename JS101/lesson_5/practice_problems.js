c0bc6e065a2e41fdedc7c611800a7ae1523c5d10
Taron Avagyan <contact@taronavagyan.com>
2022-05-17 16:58:52 -0700

    .

--- JS101/lesson_5/practice_problems.js
+++ JS101/lesson_5/practice_problems.js
@@ -200,4 +200,5 @@ console.log(sortedArr);
 // 11.
 
 let arr = [{ a: 1 }, { b: 2, c: 3 }, { d: 4, e: 5, f: 6 }];
-console.log(arr);
+let stringifiedArr = JSON.stringify(arr);
+let deepCopiedArr = JSON.parse(stringifiedArr);
