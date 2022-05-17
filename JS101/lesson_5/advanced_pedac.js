2252dcf37d7994b1329c54b85e057533377b1d56
Taron Avagyan <contact@taronavagyan.com>
2022-05-17 10:46:28 -0700

    notes

---
+++ JS101/lesson_5/advanced_pedac.js
@@ -0,0 +1,22 @@
+// 1. GET string
+// 2. Create array with all substrings of string
+// 3. Filter substrings for palindromes
+// 4. RETURN longest palindrome
+
+// 2...
+// a. Create result array
+// b. Loop through each char of the string
+// c. Starting with just the char, push the char to the array
+// d. Add the next character until the end of the string
+// e. Do for all characters
+// f. Return result array
+
+// 3...
+// a. Keep the substring if it is equal to itself reversed
+//    else, filter it out
+
+// 4...
+// a. Set the value of the longest palindrome to the length of
+//    the first substring
+// b. Loop through each substring of the array
+// c. If the length of the current word is longer, change longest to that length
