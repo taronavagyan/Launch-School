73e5d780f50d83a76be54c946e06d5b11977db52
Taron Avagyan <contact@taronavagyan.com>
2022-05-16 18:00:49 -0700

    #4

--- JS101/lesson_5/practice_problems.js
+++ JS101/lesson_5/practice_problems.js
@@ -54,3 +54,27 @@ let obj2 = { first: { d: 3 }, second: { e: 2, f: 1 }, third: { g: 0 } };
 
 console.log(Object.keys(obj2.third)[0]);
 */
+
+// 4.
+
+/*
+let arr1 = [1, [2, 3], 4];
+
+arr1[1][1] = 4;
+console.log(arr1);
+
+let arr2 = [{ a: 1 }, { b: 2, c: [7, 6, 5], d: 4 }, 3];
+
+arr2[2] = 4;
+console.log(arr2);
+
+let obj1 = { first: [1, 2, [3]] };
+
+obj1.first[2][0] = 4;
+console.log(obj1);
+
+let obj2 = { a: { a: ["1", "two", 3], b: 4 }, b: 5 };
+
+obj2.a.a[2] = 4;
+console.log(obj2);
+*/
