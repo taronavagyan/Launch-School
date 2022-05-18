615df79c6b38b287c062635fe6dd85d30c0fd366
Taron Avagyan <contact@taronavagyan.com>
2022-05-18 10:17:01 -0700

    #12

--- JS101/lesson_5/practice_problems.js
+++ JS101/lesson_5/practice_problems.js
@@ -213,3 +213,15 @@ let newArr = arr.map((obj) => {
 
 console.log(newArr);
 */
+
+// 12.
+
+/*
+let arr = [[2], [3, 5, 7], [9], [11, 15, 18]];
+
+let filteredArr = arr.map((subArray) => {
+  return subArray.filter((num) => num % 3 === 0);
+});
+
+console.log(filteredArr);
+*/
