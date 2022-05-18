d02b567593100363203316e56f7ac4d352a897e7
Taron Avagyan <contact@taronavagyan.com>
2022-05-18 11:08:43 -0700

    finished #15

--- JS101/lesson_5/practice_problems.js
+++ JS101/lesson_5/practice_problems.js
@@ -287,3 +287,21 @@ for (let key in obj) {
 }
 console.log(result);
 */
+
+// 15.
+
+/*
+let arr = [
+  { a: [1, 2, 3] },
+  { b: [2, 4, 6], c: [3, 6], d: [4] },
+  { e: [8], f: [6, 10] },
+];
+
+let newArr = arr.filter((obj) =>
+  Object.values(obj)
+    .flat()
+    .every((num) => num % 2 === 0)
+);
+
+console.log(newArr);
+*/
