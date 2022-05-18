249bb3609e7a2bb532b9163c05b832eed1b2e783
Taron Avagyan <contact@taronavagyan.com>
2022-05-18 10:12:57 -0700

    #11

--- JS101/lesson_5/practice_problems.js
+++ JS101/lesson_5/practice_problems.js
@@ -199,6 +199,17 @@ console.log(sortedArr);
 
 // 11.
 
+/*
 let arr = [{ a: 1 }, { b: 2, c: 3 }, { d: 4, e: 5, f: 6 }];
-let stringifiedArr = JSON.stringify(arr);
-let deepCopiedArr = JSON.parse(stringifiedArr);
+
+let newArr = arr.map((obj) => {
+  let incrementedObj = {};
+
+  for (let key in obj) {
+    incrementedObj[key] = obj[key] + 1;
+  }
+  return incrementedObj;
+});
+
+console.log(newArr);
+*/
