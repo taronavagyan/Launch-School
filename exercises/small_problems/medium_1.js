85da37c9318cd0f3ae547f4662737dcfed4c56b5
Taron Avagyan <contact@taronavagyan.com>
2022-05-19 16:08:15 -0700

    #1

--- exercises/small_problems/medium_1.js
+++ exercises/small_problems/medium_1.js
@@ -1,9 +1,27 @@
 // 1.
 
+/*
 function rotateArray(arr) {
   if (!Array.isArray(arr)) return undefined;
-  return [...arr];
+  if (arr.length === 0) return [];
+  let copiedArr = [...arr];
+  let firstVal = copiedArr.shift();
+  copiedArr.push(firstVal);
+  return copiedArr;
 }
 
-let array = [1, 2, 3, 4, 5];
-console.log(rotateArray(array));
+rotateArray([7, 3, 5, 2, 9, 1]); // [3, 5, 2, 9, 1, 7]
+rotateArray(["a", "b", "c"]); // ["b", "c", "a"]
+rotateArray(["a"]); // ["a"]
+rotateArray([1, "a", 3, "c"]); // ["a", 3, "c", 1]
+rotateArray([{ a: 2 }, [1, 2], 3]); // [[1, 2], 3, { a: 2 }]
+rotateArray([]); // []
+
+// return `undefined` if the argument is not an array
+rotateArray(); // undefined
+rotateArray(1); // undefined
+
+// the input array is not mutated
+let array = [1, 2, 3, 4];
+rotateArray(array); // [2, 3, 4, 1]
+*/
