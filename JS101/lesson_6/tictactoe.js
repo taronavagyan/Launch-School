ce634960890da6ebf07893cc845d296fcbda5211
Taron Avagyan <contact@taronavagyan.com>
2022-05-20 22:38:28 -0700

    display board with game

--- JS101/lesson_6/tictactoe.js
+++ JS101/lesson_6/tictactoe.js
@@ -1,13 +1,29 @@
-console.log("");
-console.log("     |     |");
-console.log("     |     |");
-console.log("     |     |");
-console.log("-----+-----+-----");
-console.log("     |     |");
-console.log("     |     |");
-console.log("     |     |");
-console.log("-----+-----+-----");
-console.log("     |     |");
-console.log("     |     |");
-console.log("     |     |");
-console.log("");
+function displayBoard(board) {
+  console.log("");
+  console.log("     |     |");
+  console.log(`  ${board["1"]}  |  ${board["2"]}  |  ${board["3"]}`);
+  console.log("     |     |");
+  console.log("-----+-----+-----");
+  console.log("     |     |");
+  console.log(`  ${board["4"]}  |  ${board["5"]}  |  ${board["6"]}`);
+  console.log("     |     |");
+  console.log("-----+-----+-----");
+  console.log("     |     |");
+  console.log(`  ${board["7"]}  |  ${board["8"]}  |  ${board["9"]}`);
+  console.log("     |     |");
+  console.log("");
+}
+
+let board = {
+  1: "X",
+  2: " ",
+  3: " ",
+  4: " ",
+  5: "X",
+  6: " ",
+  7: " ",
+  8: " ",
+  9: "O",
+};
+
+displayBoard(board);
