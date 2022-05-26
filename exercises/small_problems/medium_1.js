b5868c5dc1533676e8b1f18901166a2361a382d5
Taron Avagyan <contact@taronavagyan.com>
2022-05-25 17:40:01 -0700

    #5

--- exercises/small_problems/medium_1.js
+++ exercises/small_problems/medium_1.js
@@ -188,3 +188,30 @@ minilang("6 PUSH");
 // For every word, if the word is a digit word
 // Set it equal to the corresponding object value
 // Join the words back with " " and RETURN
+
+const NUM_WORDS = {
+  zero: 0,
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9,
+};
+
+function wordToDigit(sentence) {
+  Object.keys(NUM_WORDS).forEach((word) => {
+    let regex = new RegExp(word, "g");
+    sentence = sentence.replace(regex, NUM_WORDS[word]);
+  });
+
+  return sentence;
+}
+
+console.log(
+  wordToDigit("Please call me at five five five one two three four. Thanks.")
+);
+// "Please call me at 5 5 5 1 2 3 4. Thanks."
