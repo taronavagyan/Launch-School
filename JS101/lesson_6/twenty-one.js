ae279ded599641fc975b3aca6279aab320705f3f
Taron Avagyan <contact@taronavagyan.com>
2022-06-01 14:25:08 -0700

    added values and shuffling

--- JS101/lesson_6/twenty-one.js
+++ JS101/lesson_6/twenty-one.js
@@ -1,1 +1,31 @@
 const readline = require("readline-sync");
+
+const SUITS = ["H", "D", "S", "C"];
+const VALUES = [
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+  "A",
+];
+
+function prompt(msg) {
+  console.log(`=> {msg}`);
+}
+
+function shuffle(array) {
+  for (let first = array.length - 1; first > 0; first -= 1) {
+    let second = Math.floor(Math.random() * (first + 1));
+    [array[first], array[second]] = [array[second], array[first]];
+  }
+
+  return array;
+}
