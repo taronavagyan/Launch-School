dd26826ec0ef62b9641051ea15ebbd5f342e28b8
Taron Avagyan <contact@taronavagyan.com>
2022-06-02 22:10:39 -0700

    test

--- JS101/lesson_6/fun_tests.js
+++ JS101/lesson_6/fun_tests.js
@@ -9,5 +9,23 @@ while (true) {
 }
 */
 
+/*
 let numbers = [1, 111, 1121, 113, 123434, 23, 12, 34];
 console.log(numbers.sort((a, b) => a - b));
+*/
+
+/*
+function collapseConsecutiveChars(string) {
+  let chars = string.split("");
+  let result = "";
+
+  for (let idx = 0; idx < chars.length; idx += 1) {
+    let currentChar = chars[idx];
+    let nextChar = chars[idx + 1];
+    if (currentChar !== nextChar) result += currentChar;
+  }
+  return result;
+}
+
+console.log(collapseConsecutiveChars("hoooo1111orsssse"));
+*/
