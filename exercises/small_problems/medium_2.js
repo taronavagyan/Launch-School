ddf0221aa4d2549cda878b01f6c4b726ba0343d3
Taron Avagyan <contact@taronavagyan.com>
2022-06-07 22:13:03 -0700

    minor fix

--- exercises/small_problems/medium_2.js
+++ exercises/small_problems/medium_2.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 // 1.
 
 // PROBLEM:
@@ -667,13 +668,11 @@ let longerText =
   "shall not perish from the earth.";
 
 longestSentence(longText);
-// eslint-disable-next-line max-len
 // Four score and seven years ago our fathers brought forth on this continent a new nation, conceived in liberty, and dedicated to the proposition that all men are created equal.
 //
 // The longest sentence has 30 words.
 
 longestSentence(longerText);
-// eslint-disable-next-line max-len
 // It is rather for us to be here dedicated to the great task remaining before us -- that from these honored dead we take increased devotion to that cause for which they gave the last full measure of devotion -- that we here highly resolve that these dead shall not have died in vain -- that this nation, under God, shall have a new birth of freedom -- and that government of the people, by the people, for the people, shall not perish from the earth.
 //
 // The longest sentence has 86 words.
