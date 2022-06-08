88ad20c9d8e972923df1d14a088211209b82b674
Taron Avagyan <contact@taronavagyan.com>
2022-06-07 22:12:35 -0700

    #8

--- exercises/small_problems/medium_2.js
+++ exercises/small_problems/medium_2.js
@@ -532,3 +532,159 @@ bubbleSort(array3);
 console.log(array3);
 // ["Alice", "Bonnie", "Kim", "Pete", "Rachel", "Sue", "Tyler"]
 */
+
+// 8.
+
+// PROBLEM:
+
+// Given a string
+// Return the longest sentence in the string
+// Longest means the sentence with the most words
+// Sentences may end with a ".", "!", or "?"
+// Any sequences that are not sentence-ending characters or spaces
+//   are considered words
+
+// EXAMPLES/ TEST CASES:
+
+/*
+let longText =
+  'Four score and seven years ago our fathers brought forth on this ' +
+  'continent a new nation, conceived in liberty, and dedicated to the ' +
+  'proposition that all men are created equal. Now we are engaged in a ' +
+  'great civil war, testing whether that nation, or any nation so ' +
+  'conceived and so dedicated, can long endure. We are met on a great ' +
+  'battlefield of that war. We have come to dedicate a portion of that ' +
+  'field, as a final resting place for those who here gave their lives ' +
+  'that that nation might live. It is altogether fitting and proper that ' +
+  'we should do this.';
+
+let longerText = longText +
+  'But, in a larger sense, we can not dedicate, we can not consecrate, ' +
+  'we can not hallow this ground. The brave men, living and dead, who ' +
+  'struggled here, have consecrated it, far above our poor power to add ' +
+  'or detract. The world will little note, nor long remember what we say ' +
+  'here but it can never forget what they did here. It is for us the ' +
+  'living, rather, to be dedicated here to the unfinished work which ' +
+  'they who fought here have thus far so nobly advanced. It is rather ' +
+  'for us to be here dedicated to the great task remaining before us -- ' +
+  'that from these honored dead we take increased devotion to that ' +
+  'cause for which they gave the last full measure of devotion -- that ' +
+  'we here highly resolve that these dead shall not have died in vain ' +
+  '-- that this nation, under God, shall have a new birth of freedom -- ' +
+  'and that government of the people, by the people, for the people, ' +
+  'shall not perish from the earth.';
+
+longestSentence(longText);
+// Four score and seven years ago our fathers brought forth on this continent a
+// new nation, conceived in liberty, and dedicated to the proposition that all
+// men are created equal.
+//
+// The longest sentence has 30 words.
+
+longestSentence(longerText);
+// It is rather for us to be here dedicated to the great task remaining before
+// us -- that from these honored dead we take increased devotion to that cause
+// for which they gave the last full measure of devotion -- that we here highly
+// resolve that these dead shall not have died in vain -- that this nation,
+// under God, shall have a new birth of freedom -- and that government of the
+// people, by the people, for the people, shall not perish from the earth.
+//
+// The longest sentence has 86 words.
+
+longestSentence("Where do you think you're going? What's up, Doc?");
+// Where do you think you're going?
+//
+// The longest sentence has 6 words.
+
+longestSentence("To be or not to be! Is that the question?");
+// To be or not to be!
+//
+// The longest sentence has 6 words.
+*/
+
+// DATA STRUCTURE:
+
+// Input: One String
+// Output: One String
+// Intermediary (?): Array
+
+// ALGORITHM:
+
+// GET a string of sentences
+// Split string into array of sentences splitting by a regex for '.','!','?'
+// Find the longest sentence
+// Replace the longest sentence with "FOUND" in the string
+// Find the character after "FOUND" in the string, and append
+//   it to the longest sentence
+// Print the longest sentence and it's word length
+
+// CODE:
+
+/*
+function longestSentence(string) {
+  let sentences = string.split(/[".!?"]/g);
+  let longestSentence = sentences
+    .sort((a, b) => b.split(" ").length - a.split(" ").length)[0]
+    .split(" ")
+    .filter((word) => word !== "")
+    .join(" ");
+
+  let wordsInLongest = longestSentence.split(" ").length;
+  let newString = string.replace(longestSentence, "FOUND");
+  let punctuation = newString[newString.indexOf("FOUND") + 5];
+
+  console.log(longestSentence + punctuation);
+  console.log("");
+  console.log(`The longest sentence has ${wordsInLongest} words.`);
+}
+
+let longText =
+  "Four score and seven years ago our fathers brought forth on this " +
+  "continent a new nation, conceived in liberty, and dedicated to the " +
+  "proposition that all men are created equal. Now we are engaged in a " +
+  "great civil war, testing whether that nation, or any nation so " +
+  "conceived and so dedicated, can long endure. We are met on a great " +
+  "battlefield of that war. We have come to dedicate a portion of that " +
+  "field, as a final resting place for those who here gave their lives " +
+  "that that nation might live. It is altogether fitting and proper that " +
+  "we should do this.";
+
+let longerText =
+  longText +
+  "But, in a larger sense, we can not dedicate, we can not consecrate, " +
+  "we can not hallow this ground. The brave men, living and dead, who " +
+  "struggled here, have consecrated it, far above our poor power to add " +
+  "or detract. The world will little note, nor long remember what we say " +
+  "here but it can never forget what they did here. It is for us the " +
+  "living, rather, to be dedicated here to the unfinished work which " +
+  "they who fought here have thus far so nobly advanced. It is rather " +
+  "for us to be here dedicated to the great task remaining before us -- " +
+  "that from these honored dead we take increased devotion to that " +
+  "cause for which they gave the last full measure of devotion -- that " +
+  "we here highly resolve that these dead shall not have died in vain " +
+  "-- that this nation, under God, shall have a new birth of freedom -- " +
+  "and that government of the people, by the people, for the people, " +
+  "shall not perish from the earth.";
+
+longestSentence(longText);
+// eslint-disable-next-line max-len
+// Four score and seven years ago our fathers brought forth on this continent a new nation, conceived in liberty, and dedicated to the proposition that all men are created equal.
+//
+// The longest sentence has 30 words.
+
+longestSentence(longerText);
+// eslint-disable-next-line max-len
+// It is rather for us to be here dedicated to the great task remaining before us -- that from these honored dead we take increased devotion to that cause for which they gave the last full measure of devotion -- that we here highly resolve that these dead shall not have died in vain -- that this nation, under God, shall have a new birth of freedom -- and that government of the people, by the people, for the people, shall not perish from the earth.
+//
+// The longest sentence has 86 words.
+
+longestSentence("Where do you think you're going? What's up, Doc?");
+// Where do you think you're going?
+//
+// The longest sentence has 6 words.
+
+longestSentence("To be or not to be! Is that the question?");
+// To be or not to be!
+//
+// The longest sentence has 6 words.
+*/
