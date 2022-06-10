ff7b0bc0f85ab126554c4a359d13e26d65bd99c6
Taron Avagyan <contact@taronavagyan.com>
2022-06-10 10:53:55 -0700

    #1, #2

--- exercises/ruby_small_problems/easy2.js
+++ exercises/ruby_small_problems/easy2.js
@@ -1,4 +1,28 @@
 // 1.
 
+/*
 let age = Math.floor(Math.random() * 181) + 20;
 console.log(`Teddy is ${age} years old.`);
+*/
+
+// 2.
+
+/*
+const readline = require("readline-sync");
+const SQ_METERS_TO_SQ_FEET = 10.7639;
+
+let lengthInMeters = readline.question(
+  "Enter the length of the room in meters: "
+);
+
+let widthInMeters = readline.question(
+  "Enter the width of the room in meters: "
+);
+
+let squareMeters = (lengthInMeters * widthInMeters).toFixed(1);
+let squareFeet = (squareMeters * SQ_METERS_TO_SQ_FEET).toFixed(2);
+
+console.log(
+  `The area of the room is ${squareMeters} square meters (${squareFeet} square feet).`
+);
+*/
