ec3b1fa88807c0757e38f2500436fb59d2314660
Taron Avagyan <contact@taronavagyan.com>
2022-06-17 18:02:03 -0700

    #1

---
+++ exercises/ruby_small_problems/easy3.js
@@ -0,0 +1,92 @@
+// PROBLEM:
+
+// Get 6 numbers from the user
+// Print a message that describes whether or not
+// the 6th number appears amongst first 5
+
+// EXAMPLES/ TEST CASES:
+
+/*
+==> Enter the 1st number:
+25
+==> Enter the 2nd number:
+15
+==> Enter the 3rd number:
+20
+==> Enter the 4th number:
+17
+==> Enter the 5th number:
+23
+==> Enter the last number:
+17
+The number 17 appears in [25, 15, 20, 17, 23].
+
+
+==> Enter the 1st number:
+25
+==> Enter the 2nd number:
+15
+==> Enter the 3rd number:
+20
+==> Enter the 4th number:
+17
+==> Enter the 5th number:
+23
+==> Enter the last number:
+18
+The number 18 does not appear in [25, 15, 20, 17, 23].
+*/
+
+// DATA STRUCTURE:
+
+// Input: 6 numbers
+// Output: String
+// Intermediate (?): Array
+
+// ALGORITHM:
+
+// SET numbers to an empty array
+// Prompt the user for a number
+// Add the number to the end of the numbers array
+// Repeat 6 times
+// Take the number at the end of numbers array
+// If the numbers array includes this number say so
+// Else, say it doesnt
+
+// CODE:
+
+const readline = require("readline-sync");
+
+const numbers = [];
+
+function prompt(msg) {
+  console.log(`==> ${msg}`);
+}
+
+prompt(`Enter the 1st number`);
+numbers.push(Number(readline.question()));
+
+prompt(`Enter the 2nd number`);
+numbers.push(Number(readline.question()));
+
+prompt(`Enter the 3rd number`);
+numbers.push(Number(readline.question()));
+
+prompt(`Enter the 4th number`);
+numbers.push(Number(readline.question()));
+
+prompt(`Enter the 5th number`);
+numbers.push(Number(readline.question()));
+
+prompt(`Enter the 6th number`);
+numbers.push(Number(readline.question()));
+
+let sixthNum = numbers.pop();
+
+if (numbers.includes(sixthNum)) {
+  console.log(`The number ${sixthNum} appears in [${numbers.join(", ")}].`);
+} else {
+  console.log(
+    `The number ${sixthNum} does not appear in [${numbers.join(", ")}].`
+  );
+}
